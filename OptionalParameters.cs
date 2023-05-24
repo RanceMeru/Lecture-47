ad4dfa67c5c8d7e3444f577a2be4e56594643fc5
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-05-24 12:30:00 -0700

    Name arguements

--- OptionalParameters.cs
+++ OptionalParameters.cs
@@ -2,7 +2,14 @@
 {
     static void Main(string[] args)
     {
+        Add(5);
+        Add(5, z: 30);//lets us skip the order to manually set the value for z 
+        //it will still add in order
+    }
 
-        
+    public static void Add( int x, int y = 1, int z = 2)
+    {
+        int answer = x + y + z;
+        Console.WriteLine(answer);
     }
 }
